7566e7e4bad5f53761c581025bb6abafc02cd89b
Lars van der Ham <lars@vanderham.email>
2019-10-28 19:46:43 +0100

    Remove ingress Use actual port Create super user

--- files/run.sh
+++ files/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+echo "Start migration"
 ./manage.py migrate
-./manage.py runserver 0.0.0.0:8099
+
+echo "Migration done"
+
+echo "Createing Admin"
+
+.manage.py createsuperuser
+
+echo "Starting DEBUG Server"
+echo "DO NOT USE FOR PRODUCTION STUFF"
+
+./manage.py runserver 0.0.0.0:8800
