2a572b7b63d203843d97a0671c4eeddcc3be155c
Lars van der Ham <lars@vanderham.email>
2019-10-28 20:11:10 +0100

    Create test admin

--- files/run.sh
+++ files/run.sh
@@ -7,7 +7,7 @@ echo "Migration done"
 
 echo "Createing Admin"
 
-./manage.py createsuperuser
+./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
 
 echo "Starting DEBUG Server"
 echo "DO NOT USE FOR PRODUCTION STUFF"
