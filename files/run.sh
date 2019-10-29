4027f7e2097e242bcd09203202f8cb871058226c
Lars van der Ham <lars@vanderham.email>
2019-10-29 23:14:18 +0100

    fix user data order

--- files/run.sh
+++ files/run.sh
@@ -16,7 +16,7 @@ echo "Migration done"
 
 if [ $INITIAL == 1 ]; then
     echo "Createing Admin"
-    ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin@example.com', 'admin', 'adminpass')"
+    ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
 fi
 
 # Starting supervisord
