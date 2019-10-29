b443db6ffa5d855ee2b52edd8d96bb9f6855f30f
Lars van der Ham <lars@vanderham.email>
2019-10-29 22:33:09 +0100

    Some debug printing

--- files/run.sh
+++ files/run.sh
@@ -19,6 +19,10 @@ if [ $INITIAL == 1 ]; then
     ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin@example.com', 'admin', 'adminpass')"
 fi
 
+echo "Printing config (rights)"
+
+ls -l /config
+
 # Starting supervisord
 
 # ./manage.py runserver 0.0.0.0:8800
