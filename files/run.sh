4d0ca0c001e6da83e6b18b659b7a00c6362014c6
Lars van der Ham <lars@vanderham.email>
2019-11-29 23:01:29 +0100

    Remove ipv6 from nginx

--- files/run.sh
+++ files/run.sh
@@ -32,21 +32,21 @@ if [ $INITIAL == 1 ]; then
     GENERATEDPASSWORD=""
 fi
 
-
+bashio::config.require.ssl
 if bashio::config.true 'ssl'; then
   # Setup ssl in nginx
-  sed -i 's#%%portandmode%%#443 ssl#g' /etc/nginx/nginx.conf
+  sed 's#%%portandmode%%#443 ssl#g' /etc/nginx/nginx.conf
 
   CERTFILE=$(bashio::config 'certfile')
   KEYFILE=$(bashio::config 'keyfile')
 
-  sed -i 's#%%certificatefile%%#${CERTFILE}#g' /etc/nginx/nginx.conf
-  sed -i 's#%%certificatekeyfile%%#${KEYFILE}#g' /etc/nginx/nginx.conf
+  sed 's#%%certificatefile%%#${CERTFILE}#g' /etc/nginx/nginx.conf
+  sed 's#%%certificatekeyfile%%#${KEYFILE}#g' /etc/nginx/nginx.conf
 else
   # Setup http ports in nginx
-  sed -i 's#%%portandmode%%#80 default#g' /etc/nginx/nginx.conf
-  sed -i "s#ssl_certificate /ssl/%%certificatefile%%##g"
-  sed -i "s#ssl_certificate_key /ssl/%%certificatekeyfile%%##g"
+  sed 's#%%portandmode%%#80 default#g' /etc/nginx/nginx.conf
+  sed "s#ssl_certificate /ssl/%%certificatefile%%##g"
+  sed "s#ssl_certificate_key /ssl/%%certificatekeyfile%%##g"
 fi
 
 # Start supervisord
